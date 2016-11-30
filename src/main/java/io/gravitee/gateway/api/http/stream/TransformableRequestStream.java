00f601c16a2641a7f9cc6030f3a913add2422dd2
brasseld <brasseld@gmail.com>
2016-11-30 18:23:35 +0100

    feat(gateway): Add a transformable request content builder
    Closes gravitee-io/issues#350

---
+++ src/main/java/io/gravitee/gateway/api/http/stream/TransformableRequestStream.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.api.http.stream;
+
+import io.gravitee.common.http.HttpHeaders;
+import io.gravitee.gateway.api.Request;
+import io.gravitee.gateway.api.buffer.Buffer;
+import io.gravitee.gateway.api.stream.TransformableStream;
+import io.gravitee.gateway.api.stream.exception.TransformationException;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public abstract class TransformableRequestStream extends TransformableStream {
+
+    private final Request request;
+
+    public TransformableRequestStream(Request request) {
+        this(request, -1);
+    }
+
+    public TransformableRequestStream(Request request, int contentLength) {
+        super(contentLength);
+        this.request = request;
+    }
+
+    @Override
+    public void end() {
+        Buffer content;
+
+        try {
+            content = transform().apply(buffer);
+
+            // Set content length (remove useless transfer encoding header)
+            request.headers().remove(HttpHeaders.TRANSFER_ENCODING);
+            request.headers().set(HttpHeaders.CONTENT_LENGTH, Integer.toString(content.length()));
+
+            // Set the content-type if settled
+            String contentType = to();
+            if (contentType != null && !contentType.isEmpty()) {
+                request.headers().set(HttpHeaders.CONTENT_TYPE, contentType);
+            }
+        } catch (TransformationException tex) {
+            content = Buffer.buffer(tex.getMessage());
+        }
+
+        super.flush(content);
+        super.end();
+    }
+
+    protected abstract String to();
+}
