c6ff0e31699345632f1a476ba246b664faeb43ed
Marc <marc.cambier@graviteesource.com>
2022-06-16 14:51:30 +0200

    feat: add services to manage api keys and subscriptions

---
+++ src/main/java/io/gravitee/gateway/api/service/SubscriptionService.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.api.service;
+
+import java.util.Optional;
+
+/**
+ * This manages subscriptions.
+ */
+public interface SubscriptionService {
+    /**
+     * Save the given subscription.
+     * Will put it in cache if active, or remove it from cache elsewhere.
+     *
+     * @param subscription the subscription to save
+     */
+    void save(Subscription subscription);
+
+    /**
+     * Get subscription by its API and client ID.
+     *
+     * @param api Searched API
+     * @param clientId Searched client ID
+     * @return Found subscription
+     */
+    Optional<Subscription> getByApiAndClientId(String api, String clientId);
+
+    /**
+     * Get subscription by its ID.
+     *
+     * @param subscriptionId Searched subscription ID
+     * @return Found subscription
+     */
+    Optional<Subscription> getById(String subscriptionId);
+}
