f0fc253756e5e2b552e1fa05755df7ce05d8ab2e
Guillaume Lamirand <guillaume.lamirand@graviteesource.com>
2023-02-14 07:35:41 +0100

    fix: add missing no args constructor on SubscriptionConfiguration

--- src/main/java/io/gravitee/gateway/api/service/SubscriptionConfiguration.java
+++ src/main/java/io/gravitee/gateway/api/service/SubscriptionConfiguration.java
@@ -15,14 +15,16 @@
  */
 package io.gravitee.gateway.api.service;
 
-import java.util.Date;
-import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Builder
