6096a74aef473f8124804d78d696af05cf1d3bdc
eric <eric.leleu@graviteesource.com>
2023-03-09 14:47:55 +0100

    Revert "chore: rename 'jupiter' package in 'reactive'"
    This reverts commit 45922c4ae55879d6fb3a6e69ebc41e4d33be66ed.

---
+++ src/main/java/io/gravitee/gateway/jupiter/api/ListenerType.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.jupiter.api;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RequiredArgsConstructor
+@Getter
+public enum ListenerType {
+    HTTP("http"),
+    SUBSCRIPTION("subscription"),
+    TCP("tcp");
+
+    private static final Map<String, ListenerType> LABELS_MAP = Map.of(HTTP.label, HTTP, SUBSCRIPTION.label, SUBSCRIPTION, TCP.label, TCP);
+
+    @JsonValue
+    private final String label;
+
+    public static ListenerType fromLabel(final String label) {
+        if (label != null) {
+            return LABELS_MAP.get(label);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/io/gravitee/gateway/jupiter/api/connector/endpoint/async/EndpointAsyncConnector.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.jupiter.api.connector.endpoint.async;
+
+import static io.gravitee.gateway.jupiter.api.context.InternalContextAttributes.ATTR_INTERNAL_ENTRYPOINT_CONNECTOR;
+
+import io.gravitee.common.service.AbstractService;
+import io.gravitee.gateway.jupiter.api.ApiType;
+import io.gravitee.gateway.jupiter.api.ConnectorMode;
+import io.gravitee.gateway.jupiter.api.connector.Connector;
+import io.gravitee.gateway.jupiter.api.connector.endpoint.EndpointConnector;
+import io.gravitee.gateway.jupiter.api.context.ExecutionContext;
+import io.gravitee.gateway.jupiter.api.qos.Qos;
+import io.gravitee.gateway.jupiter.api.qos.QosCapability;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.CompletableSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Specialized {@link EndpointConnector} for {@link ApiType#ASYNC}
+ */
+public abstract class EndpointAsyncConnector extends AbstractService<Connector> implements EndpointConnector {
+
+    @Override
+    public ApiType supportedApi() {
+        return ApiType.ASYNC;
+    }
+
+    /**
+     * Returns a set of {@link Qos} supported by this connector.
+     *
+     * @return set of {@link Qos} supported by this connector.
+     */
+    public abstract Set<Qos> supportedQos();
+
+    /**
+     * Returns a set of {@link QosCapability} supported by this connector.
+     *
+     * @return set of {@link QosCapability} supported by this connector.
+     */
+    public abstract Set<QosCapability> supportedQosCapabilities();
+
+    @Override
+    public Completable connect(final ExecutionContext ctx) {
+        return Completable.defer(() -> {
+            List<CompletableSource> completableList = new ArrayList<>();
+            Connector entrypointConnector = ctx.getInternalAttribute(ATTR_INTERNAL_ENTRYPOINT_CONNECTOR);
+            if (entrypointConnector != null) {
+                if (entrypointConnector.supportedModes().contains(ConnectorMode.PUBLISH)) {
+                    completableList.add(publish(ctx));
+                }
+                if (entrypointConnector.supportedModes().contains(ConnectorMode.SUBSCRIBE)) {
+                    completableList.add(subscribe(ctx));
+                }
+            }
+            return Completable.merge(completableList);
+        });
+    }
+
+    public abstract Completable subscribe(final ExecutionContext ctx);
+
+    public abstract Completable publish(final ExecutionContext ctx);
+}
