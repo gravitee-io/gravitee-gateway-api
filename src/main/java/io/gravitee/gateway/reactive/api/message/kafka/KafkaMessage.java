efb63e8c6d43725c2647dda4afc27f83d9a29e48
ThibaudAv <thibaud.avenier@gmail.com>
2024-10-28 15:32:53 +0100

    feat: add property for KafkaMessage interfaces

--- src/main/java/io/gravitee/gateway/reactive/api/message/kafka/KafkaMessage.java
+++ src/main/java/io/gravitee/gateway/reactive/api/message/kafka/KafkaMessage.java
@@ -18,8 +18,58 @@ package io.gravitee.gateway.reactive.api.message.kafka;
 import io.gravitee.gateway.api.buffer.Buffer;
 import io.gravitee.gateway.api.http.HttpHeaders;
 import io.gravitee.gateway.reactive.api.message.Message;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-public interface KafkaMessage extends Message {}
+public interface KafkaMessage extends Message {
+    /**
+     * FIXME: Refactor/remove this method when {@link Message#headers() } are http agnostic
+     * Get the Kafka record headers associated to the message.
+     * @return the Kafka headers
+     */
+    Map<String, Object> recordHeaders();
+
+    /**
+     * Get the Kafka record key associated to the message.
+     * @return the Kafka key
+     */
+    Buffer key();
+
+    /**
+     * Get the Kafka record offset value associated to the message.
+     * @return the Kafka offset
+     */
+    long offset();
+
+    /**
+     * Get the Kafka record partition value associated to the message.
+     * @return the Kafka partition
+     */
+    int sequence();
+
+    /**
+     * Get the Kafka record indexPartition type value associated to the message.
+     * @return the Kafka indexPartition
+     */
+    int indexPartition();
+
+    /**
+     * Get the Kafka record topic value associated to the message.
+     * @return the Kafka topic
+     */
+    String topic();
+
+    @Deprecated
+    default void ack() {
+        throw new IllegalStateException("Ack not supported for Kafka messages");
+    }
+
+    @Deprecated
+    default HttpHeaders headers() {
+        throw new IllegalStateException("HttpHeaders not supported for Kafka messages");
+    }
+
+    @Deprecated
+    default Message headers(HttpHeaders headers) {
+        throw new IllegalStateException("HttpHeaders not supported for Kafka messages");
+    }
+}
