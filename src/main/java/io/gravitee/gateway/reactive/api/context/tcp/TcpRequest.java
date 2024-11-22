2439549e517a4dd6bd5a79c27a36d2b82cea34de
ytvnr <yann.tavernier@graviteesource.com>
2024-11-22 09:29:02 +0100

    feat: add chunks capabilities for Tcp Request and Response https://gravitee.atlassian.net/browse/APIM-7417

--- src/main/java/io/gravitee/gateway/reactive/api/context/tcp/TcpRequest.java
+++ src/main/java/io/gravitee/gateway/reactive/api/context/tcp/TcpRequest.java
@@ -15,8 +15,10 @@
  */
 package io.gravitee.gateway.reactive.api.context.tcp;
 
+import io.gravitee.gateway.api.buffer.Buffer;
 import io.gravitee.gateway.reactive.api.context.base.BaseRequest;
 import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Flowable;
 
 /**
  * Request specialized for TCP allowing piping traffic from client to backend.
@@ -30,4 +32,39 @@ public interface TcpRequest extends BaseRequest {
      * @param pipe the completable to perform the action
      */
     void pipeUpstream(Completable pipe);
+
+    /**
+     * Set the current request body chunks from a {@link Flowable} of {@link Buffer}.
+     *
+     * <p>
+     * This is useful to directly pump the upstream chunks to the downstream without having to load all the chunks in memory.
+     * </p>
+     *
+     * <p>
+     * <b>WARN:</b>
+     * <ul>
+     *  <li>replacing the request chunks will <b>NOT</b> "drain" the previous request that was in place.</li>
+     *  <li>You <b>MUST</b> ensure to consume the previous chunks by yourself when using it.</li>
+     * </ul>
+     *
+     * </p>
+     *
+     * @param chunks the {@link Flowable} of chunks representing the request body that will be pushed to the upstream.
+     */
+    void chunks(final Flowable<Buffer> chunks);
+
+    /**
+     * Get the current request body chunks as {@link Flowable} of {@link Buffer}.
+     *
+     * <p>
+     * This is useful when you want to manipulate the entire body without having to load it in memory.
+     * </p>
+     *
+     * <p>
+     * <b>WARN:</b> you should not keep a direct reference on the body chunks as they could be overridden by others at anytime.
+     * </p>
+     *
+     * @return a {@link Flowable} containing the current body request chunks.
+     */
+    Flowable<Buffer> chunks();
 }
