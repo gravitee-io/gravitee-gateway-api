5876e68fc18e6d06b0f7d963205a7940abb0c475
Michel Barret <michel.barret@graviteesource.com>
2025-10-15 15:14:57 +0200

    feat: create new LLM proxy interfaces
    https://gravitee.atlassian.net/browse/APIM-11457

---
+++ src/main/java/io/gravitee/gateway/reactive/api/connector/entrypoint/sync/LLMProxyEntrypointSyncConnector.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.gateway.reactive.api.connector.entrypoint.sync;
+
+import io.gravitee.gateway.reactive.api.ApiType;
+import io.gravitee.gateway.reactive.api.ConnectorMode;
+import io.gravitee.gateway.reactive.api.ListenerType;
+import io.gravitee.gateway.reactive.api.connector.entrypoint.HttpEntrypointConnector;
+import java.util.Set;
+
+/**
+ * Specialized {@link HttpEntrypointConnector} for {@link ApiType#LLM_PROXY}
+ */
+public interface LLMProxyEntrypointSyncConnector extends HttpEntrypointConnector {
+    @Override
+    default ApiType supportedApi() {
+        return apiType();
+    }
+
+    @Override
+    default Set<ConnectorMode> supportedModes() {
+        return modes();
+    }
+
+    @Override
+    default ListenerType supportedListenerType() {
+        return listenerType();
+    }
+
+    static ApiType apiType() {
+        return ApiType.LLM_PROXY;
+    }
+
+    static Set<ConnectorMode> modes() {
+        return Set.of(ConnectorMode.REQUEST_RESPONSE);
+    }
+
+    static ListenerType listenerType() {
+        return ListenerType.HTTP;
+    }
+}
