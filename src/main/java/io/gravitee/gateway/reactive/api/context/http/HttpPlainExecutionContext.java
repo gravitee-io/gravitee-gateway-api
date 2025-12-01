gravitee-io/gravitee-gateway-api

commit 68e499caaaf608684a3dac4d393f257728e7f304

feat: add action that will be executed at http request response phase
'addActionOnResponse' allows registering a callback that will be executed during the response phase.
This enables a policy to schedule work on the response while still inside its 'onRequest' method of the policy.

src/main/java/io/gravitee/gateway/reactive/api/context/http/HttpPlainExecutionContext.java
@@ -19,6 +19,7 @@ import io.gravitee.gateway.api.buffer.Buffer;
 import io.gravitee.gateway.reactive.api.ExecutionFailure;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Maybe;
+import java.util.function.Function;
 
 /**
  * Http execution context allowing access to {@link HttpPlainRequest} and {@link HttpPlainResponse} that manipulates raw http bodies (content or chunks).
@@ -65,4 +66,12 @@ public interface HttpPlainExecutionContext extends HttpBaseExecutionContext {
      * Same as {@link #interruptBody()} but with an {@link ExecutionFailure} object that indicates that the execution has failed. The {@link ExecutionFailure} can be processed in order to build a proper response (ex: based on templating, with appropriate accept-encoding, ...).
      */
     Maybe<Buffer> interruptBodyWith(final ExecutionFailure failure);
+
+    /**
+     * Registers an action defined during the request phase to be executed later during the response phase.
+     * When multiple actions are registered, they are executed in reverse order of registration (LIFO).
+     *
+     * @param onResponseCallback the action to be executed during the response phase
+     */
+    void addActionOnResponse(Function<HttpExecutionContext, Completable> onResponseCallback);
 }

commit 6efbe2935d4bfee6bb4f920520db3865c7461978

fix: add default implementation
Avoid breaking change introduced in commit: 68e499caaaf608684a3dac4d393f257728e7f304

src/main/java/io/gravitee/gateway/reactive/api/context/http/HttpPlainExecutionContext.java
@@ -73,5 +73,7 @@ public interface HttpPlainExecutionContext extends HttpBaseExecutionContext {
      *
      * @param onResponseCallback the action to be executed during the response phase
      */
-    void addActionOnResponse(Function<HttpExecutionContext, Completable> onResponseCallback);
+    default void addActionOnResponse(Function<HttpExecutionContext, Completable> onResponseCallback) {
+        // Nothing is registered by default
+    }
 }