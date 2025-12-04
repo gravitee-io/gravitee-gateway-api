3f3d4cf9574aa774b6bdf3e6c146caa46028ed5a
Michel Barret <michel.barret@graviteesource.com>
2025-12-04 17:04:42 +0100

    chore: use BasePolicy instead of Object to have a less error prone API

--- src/main/java/io/gravitee/gateway/reactive/api/context/http/HttpPlainExecutionContext.java
+++ src/main/java/io/gravitee/gateway/reactive/api/context/http/HttpPlainExecutionContext.java
@@ -17,6 +17,7 @@ package io.gravitee.gateway.reactive.api.context.http;
 
 import io.gravitee.gateway.api.buffer.Buffer;
 import io.gravitee.gateway.reactive.api.ExecutionFailure;
+import io.gravitee.gateway.reactive.api.policy.base.BasePolicy;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Maybe;
 import java.util.function.Function;
@@ -71,10 +72,10 @@ public interface HttpPlainExecutionContext extends HttpBaseExecutionContext {
      * Registers an action defined during the request phase to be executed later during the response phase.
      * When multiple actions are registered, they are executed in reverse order of registration (LIFO).
      *
-     * @param source the object that initiated the action.
+     * @param source the policy that initiated the action.
      * @param onResponseCallback the action to be executed during the response phase, receiving the HTTP execution context and returning a Completable.
      */
-    default void addActionOnResponse(Object source, Function<HttpExecutionContext, Completable> onResponseCallback) {
+    default void addActionOnResponse(BasePolicy source, Function<HttpExecutionContext, Completable> onResponseCallback) {
         // Nothing is registered by default
     }
 }
